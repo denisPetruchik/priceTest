667a9bd0ced6ba291abc2a97876291d403e13569
Denis Petruchik <denis.petruchik@me.com>
2017-07-05 20:34:29 +0200

    new

--- sreality/sreality/spiders/praha.py
+++ sreality/sreality/spiders/praha.py
@@ -12,38 +12,98 @@ import json
 
 class product_spiderItem(scrapy.Item):
     id = scrapy.Field()
-    stitky = scrapy.Field()
-    pudorys = scrapy.Field()
+    stitky = scrapy.Field()    
     price = scrapy.Field()
+    url = scrapy.Field()
+    location = scrapy.Field()
+    vlastnictvi = scrapy.Field()
+    stavba = scrapy.Field()
+    navic = scrapy.Field()
+    velikost = scrapy.Field()
+    plocha = scrapy.Field()
+    podlazi = scrapy.Field()
+    novostavba = scrapy.Field()
     pass
+
 
 class PrahaSpider(CrawlSpider):
     name = 'praha'
     allowed_domains = ['sreality.cz']
     start_urls = ['https://www.sreality.cz/hledani/prodej/byty/praha']
-
+    url_strip = 'https://www.sreality.cz/detail/prodej/byt/'
+    
 
     def __init__(self):
         CrawlSpider.__init__(self)
         # use any browser you wish
-        self.browser = webdriver.Chrome('~/Downloads/chromedriver') 
+        self.browser = webdriver.Chrome(
+            '/Users/denispetruchik/Downloads/chromedriver')
 
     def __del__(self):
-        self.browser.close()
+    	print 'close'
+        self.browser.quit()
 
     def parse(self, response):
         self.browser.get(response.url)
         # let JavaScript Load
-        test_element = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "ng-scope")))
-        for i in self.browser.find_elements_by_class_name('property'): 
-            item = product_spiderItem() 
-            dod = i.get_attribute('data-dot-data')
-            data = json.loads(dod)
-            item['id'] = data['id']
-            item['stitky'] = data['stitky']
-            item['pudorys'] = data['pudorys']
-            yield item
+        test_element = WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "ng-scope")))
 
+        items = []
+        for i in self.browser.find_elements_by_class_name('property'):
+            self.parse_byt(i, items)
+            
+        nextPage = 2
+        page_to_parse = 10
+        while (nextPage < page_to_parse):
+            self.browser.get(response.url + '?strana=' + str(nextPage))
+            # let JavaScript Load
+            test_element = WebDriverWait(self.browser, 10).until(
+                EC.visibility_of_element_located((By.CLASS_NAME, "ng-scope")))
+            for i in self.browser.find_elements_by_class_name('property'):
+                self.parse_byt(i, items)
+            nextPage += 1
 
+        for i in items:
+            yield self.parse_details(i['url'], i)          
 
-        
+    def parse_byt(self, i, items):
+        item = product_spiderItem()
+        dod = i.get_attribute('data-dot-data')
+        data = json.loads(dod)
+        item['id'] = data.get('id')
+        item['stitky'] = data.get('stitky')        
+        details_link = i.find_element_by_css_selector('a.images')
+        url = details_link.get_attribute('href')
+        item['url'] = url.replace(self.url_strip, '')
+        items.append(item)
+
+    def parse_details(self, url, i):
+        self.browser.get(self.url_strip + url)
+        self.browser.implicitly_wait(5)
+        test_element = WebDriverWait(self.browser, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "norm-price")))
+        i['price'] = self.browser.find_element_by_css_selector('span.norm-price').text
+        i['location'] = self.browser.find_element_by_css_selector('span.location-text').text
+        #okres&quot;:&quot;praha-10&quot;,&quot;vlastnictvi&quot;:&quot;druzstevni&quot;,&quot;stavba&quot;:&quot;cihlova&quot;,&quot;navic&quot;:&quot;sklep,vytah&quot;,&quot;typ-nemovitosti&quot;:&quot;byty&quot;,&quot;typ-transakce&quot;:&quot;prodej&quot;,&quot;velikost&quot;:&quot;2+1&quot;}">
+        dod = self.browser.find_element_by_css_selector('div.buttons').get_attribute('data-dot-data')
+        data = json.loads(dod)
+        i['vlastnictvi'] = data.get('vlastnictvi','')
+        i['stavba'] = data.get('stavba','')
+        i['navic'] = data.get('navic', '')
+        i['velikost'] = data.get('velikost','')
+        i['novostavba'] = 'False'
+
+        addPars = ''
+        nextParse = False
+        for par in self.browser.find_elements_by_xpath("//strong[@class='param-value']/span[1]"):
+            text = par.text
+            if nextParse :
+                i['plocha'] = text
+                nextParse = False
+            if text.find(u'Novostavba') > -1:
+                i['novostavba'] = 'True'
+            if text.find(u'podlaží z celkem') > -1:
+                i['podlazi'] = text
+                nextParse = True
+        return i
--- start_scrapy.sh
+++ start_scrapy.sh
@@ -1,3 +1,3 @@
 source ~/myVirt/bin/activate
-cd $(dirname $0)
-/Users/denispetruchik/myVirt/bin/scrapy 
+cd $(dirname $0)/sreality
+/Users/denispetruchik/myVirt/bin/scrapy crawl praha -o output.json
